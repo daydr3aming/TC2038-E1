8c18d7ea60b8e712c19c136a440f28f7be95da41
DavidGlezI <88803550+DavidGlezI@users.noreply.github.com>
2023-09-20 23:23:07 -0600

    Parte 1

--- ACTIntegradora.cpp
+++ ACTIntegradora.cpp
@@ -1,10 +1,102 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <vector>
+#include <string.h>
 
 // Actividad Integradora: A01639914 y A01633817
+
+
+void ArregloLPS(char const* patron, int size, int *arreglo){ // arreglo
+    int len = 0;
+    arreglo[0] = 0;
+    int i = 1;
+
+    while(i<size){
+        if(patron[i]==patron[len]){
+            len++;
+            arreglo[i] = len;
+            i++;
+        }
+        else{
+            if(len != 0 ){
+                len = arreglo[len - 1];
+            }
+            else{
+                arreglo[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+
+void KMPSearch(char const* patron, char const* palabra, int m, int t){
+    int M = strlen(patron);
+    int N = strlen(palabra);
+
+    int lpsArr[M];
+
+    ArregloLPS(patron, M, lpsArr);
+
+    int i = 0;
+    int j = 0;
+    while(i < N){
+        if(patron[j] == palabra[i]){
+            j++;
+            i++;
+        }
+
+        if(j == M){
+            printf("True, transmission %d con mcode %d : patron en el indice: %d \n", t, m, i-j);
+            j = lpsArr[j-1];
+
+        }
+        else if( i < N && patron[j]!= palabra[i]){
+
+            if( j != 0){
+                j = lpsArr[j-1];
+            }
+            else{
+                i = i + 1;
+            }
+        }
+    }
+}
+
+void parte1(std::vector<std::string> mcode, std::vector<std::string> transmission, int tNumber, int mNumber){
+
+
+
+    for(int i=0; i<transmission.size(); i++){
+
+        const int length =  transmission[i].length();
+        char* tranPalabra = new char[length + 1];
+        strcpy(tranPalabra,  transmission[i].c_str());
+
+
+        for(int j=0; j< mcode.size(); j++){
+            
+            const int length2 =  mcode[j].length();
+            char* mcodePalabra = new char[length2 + 1];
+            strcpy(mcodePalabra,  mcode[j].c_str());
+
+
+            // Aqui ya tenemos en variables, la palabra de transmission y mcode, solo falta comparar
+
+  
+            KMPSearch(mcodePalabra, tranPalabra, mNumber, tNumber);
+
+
+            delete[] mcodePalabra;
+        }
+
+        delete[] tranPalabra;
+    }
+
+}
 
 int main(){
     std::fstream transmission1file("transmission1.txt");
@@ -22,35 +114,36 @@ int main(){
 
 
     std::string line;
-    std::string transmission1;
-    std::string transmission2;
-    std::string transmission3;
-    std::string mcode1;
-    std::string mcode2;
-    std::string mcode3;
-
+    std::vector<std::string> transmission1;
+    std::vector<std::string> transmission2;
+    std::vector<std::string> transmission3;
+    std::vector<std::string> mcode1;
+    std::vector<std::string> mcode2;
+    std::vector<std::string> mcode3;
 
     for(int i = 0; i < allfiles.size(); i++){
         std::fstream file(allfiles[i] + ".txt");
+
         while(getline(file, line)){
             if(i == 0){
-                transmission1 += line + "\n";
+                transmission1.push_back(line);
             }
             else if(i == 1){
-                transmission2 += line + "\n";
+                transmission2.push_back(line);
             }
             else if(i == 2){
-                mcode1 += line + "\n";
+                mcode1.push_back(line);
             }
             else if(i == 3){
-                mcode2 += line + "\n";
+                mcode2.push_back(line);
             }
             else if(i == 4){
-                mcode3 += line + "\n";
+                mcode3.push_back(line);
             }
         }
     }
 
+    /*
     std::cout << "tranmission1: " << std::endl;
     std::cout << transmission1 << std::endl;
     std::cout << "tranmission2: " << std::endl;
@@ -61,12 +154,32 @@ int main(){
     std::cout << mcode2 << std::endl;
     std::cout << "mcode3: " << std::endl;
     std::cout << mcode3 << std::endl;
+*/
+
+
+   // parte1(mcode1, transmission1, 1, 1);
+    //parte1(mcode2, transmission1, 1, 2);
+
+
+    parte1(mcode3, transmission1, 1, 3);
+    /*
+    parte1(mcode1, transmission2, 2, 1);
+    parte1(mcode2, transmission2, 2, 2);
+    parte1(mcode3, transmission2, 2, 3);
+    */
+
+    //KMPSearch("znxc1m34", "znxc1m34jn413asd", 1,1);
     
     transmission1file.close();
     transmission2file.close(); 
     mcode1file.close();
     mcode2file.close();
     mcode3file.close();
+
+
+
+
+
     return 0;
 }
  
---
+++ test.cpp
@@ -0,0 +1,77 @@
+
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+
+
+void LPS(char const*pat, int M, int *lps){
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+
+    while(i<M){
+        if(pat[i]==pat[len]){
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else{
+            if(len != 0 ){
+                len = lps[len - 1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+
+
+    }
+}
+
+
+void KMPSearch(char const* pat, char const* text){
+    int M = strlen(pat);
+    int N = strlen(text);
+
+    int lps[M];
+
+    LPS(pat, M, lps);
+
+    int i = 0;
+    int j = 0;
+    while(i < N){
+        if(pat[j] == text[i]){
+            j++;
+            i++;
+        }
+
+        if(j == M){
+            printf("Found pattern at index %d \n", i-j);
+            j = lps[j-1];
+
+        }
+
+        else if( i < N && pat[j]!= text[i]){
+
+            if( j != 0){
+                j = lps[j-1];
+            }
+            else{
+                i = i + 1;
+            }
+        }
+    }
+}
+int main(){
+
+    char const* pal = "ABXAB";
+    char const* texto = "ABXABABXAB";
+
+    KMPSearch(pal, texto);
+
+
+    return 0;
+}
